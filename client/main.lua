e0f41fce14a09bd1e1b07a0427e1649459b7b654
ParentPartner <168586780+ParentPartner@users.noreply.github.com>
2024-07-04 15:59:42 -0400

    Error handling

--- client/main.lua
+++ client/main.lua
@@ -4,19 +4,36 @@
 RegisterNetEvent('apx:teleportPlayer')
 AddEventHandler('apx:teleportPlayer', function(x, y, z)
     local playerPed = PlayerPedId()
-    SetEntityCoords(playerPed, x, y, z, false, false, false, true)
+    if playerPed then
+        SetEntityCoords(playerPed, x, y, z, false, false, false, true)
+    else
+        print("Error: Failed to get playerPed.")
+    end
 end)
 
 -- Notify server that player has spawned
 AddEventHandler('playerSpawned', function()
-    TriggerServerEvent('apx:registerPlayer')
+    local playerId = PlayerId()
+    if playerId then
+        TriggerServerEvent('apx:registerPlayer')
+    else
+        print("Error: Failed to get playerId.")
+    end
 end)
 
 -- Notify server to save player location when they disconnect
 AddEventHandler('onClientResourceStop', function(resourceName)
     if resourceName == GetCurrentResourceName() then
         local playerPed = PlayerPedId()
-        local coords = GetEntityCoords(playerPed)
-        TriggerServerEvent('apx:savePlayerLocation', coords.x, coords.y, coords.z)
+        if playerPed then
+            local coords = GetEntityCoords(playerPed)
+            if coords then
+                TriggerServerEvent('apx:savePlayerLocation', coords.x, coords.y, coords.z)
+            else
+                print("Error: Failed to get player coordinates.")
+            end
+        else
+            print("Error: Failed to get playerPed.")
+        end
     end
 end)
